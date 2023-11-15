d92f523504c38ba204a2e42080d9ffb504a97af8
Serdar Ulutas <ulut0002@algonquinlive.com>
2023-11-15 00:07:19 -0500

    Add list tile widgets

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
 
+class FontData {
+  Icon icon;
+  String text;
+  TextStyle textStyle;
+
+  FontData(this.icon, this.text, this.textStyle);
+}
+
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({Key? key}) : super(key: key);
+  var borderRadius = const BorderRadius.only(
+      topRight: Radius.circular(32), bottomRight: Radius.circular(32));
+
+  static const assets = [
+    "images/photo1.jpeg",
+    "images/photo2.jpeg",
+    "images/photo3.jpeg"
+  ];
+
+  Widget _buildListItem(String title, IconData icon, String fontFamily,
+      FontStyle? fontStyle, double fontSize,
+      {FontWeight? fontWeight}) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 4),
+      padding: const EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: ListTile(
+        leading: Icon(icon),
+        title: Text(
+          title,
+          style: TextStyle(
+            fontFamily: fontFamily,
+            fontStyle: fontStyle,
+            fontWeight: fontWeight,
+            fontSize: fontSize,
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +57,47 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          children: <Widget>[
+            SizedBox(
+              height: 200, // Adjust the height as needed
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: assets.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(24),
+                      child: Image(image: AssetImage(assets[index])),
+                    ),
+                  );
+                },
+              ),
+            ),
+            _buildListItem("BeVietnamPro MediumItalic", Icons.favorite,
+                "BeVietnamBro", FontStyle.italic, 20),
+            _buildListItem("BeVietnamPro Medium", Icons.access_alarm_outlined,
+                "BeVietnamBro", FontStyle.normal, 20),
+            _buildListItem(
+                "BeVietnamPro Bold", Icons.ac_unit, "BeVietnamBro", null, 20,
+                fontWeight: FontWeight.bold),
+            _buildListItem("Chalkboard SE", Icons.accessibility_outlined,
+                "Chalkboard SE", null, 26),
+            _buildListItem(
+                "Noteworthy Italic",
+                Icons.format_align_center_outlined,
+                "Noteworthy",
+                FontStyle.italic,
+                26),
+            _buildListItem("Rockwell Bold", Icons.soap, "Rockwell", null, 26,
+                fontWeight: FontWeight.bold),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.lightBlue,
         ),
       ),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
